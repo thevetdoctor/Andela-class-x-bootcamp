9c427ce47d874a6aa15eedfdf3c4dc68d7f3f250
thevetdoctor <thevetdoctor@gmail.com>
2015-07-07 11:39:44 -0700

    updated my variables

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 var naijaArtists = ["Tuface","9ice","Timaya","Olamide"];
+Array.protoype.trending = function() {
+	for (i = 0; i < this.length; i++)
+	console.log([] + " is trending at the moment in Naija!");
+};
+naija.trending();
+
+var church = [];
+church.push("RCCG","Christ Embassy","Anglican Church","Catholic Church");
+
+naijaArtists.concat(church);
+
